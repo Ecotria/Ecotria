442236410935fd4f363e3f59849b6fd3f2de3cb0
NoelMiranda1 <mirandantonionoel@gmail.com>
2022-01-26 21:18:10 -0600

    Add Separator and images

--- src/components/Listing/Listing.jsx
+++ src/components/Listing/Listing.jsx
@@ -5,8 +5,11 @@ import { makeStyles } from "@material-ui/styles";
 import React from "react";
 import { useSelector } from "react-redux";
 import { formatDate } from "../../utils/funtions";
-
+import ImageNotFound from "../../img/papas.jpeg";
 import Nav from "../Navbar/Nav";
+import Separator from "../common/separator.component";
+import Button from '@material-ui/core/Button'
+import { Link, Redirect } from "react-router-dom";
 
 export default function Listing(props) {
   const styles = useStyles();
@@ -19,60 +22,52 @@ export default function Listing(props) {
         <h1 className={styles.title}>{details.titlePost}</h1>
       </div>
       <div className={styles.body}>
-        {/* <ListItem>
-          <h2 className={styles.boldletter}>Categoria: </h2>
-          <ListItemText primary={details.category} />
-        </ListItem>
-        <ListItem>
-          <h2 className={styles.boldletter}>Telefono: </h2>
-          <ListItemText primary={details.phoneNumber} />
-        </ListItem>
-        <ListItem>
-          <h2 className={styles.boldletter}>Precio: </h2>
-          <ListItemText primary={details.price} />
-        </ListItem>
-        <ListItem style={{top:-15}}>
-          <h2 className={styles.boldletter}>Descripcion: </h2>
-          <ListItemText style={{ marginTop: 30 }}  primary={details.descriptionPost} />
-        </ListItem>
-        <ListItem>
-          <h2 className={styles.boldletter}>Creada: </h2>
-          <ListItemText primary={formatDate(details.createDate)} />
-        </ListItem>
-        <ListItem>
-          <h2 className={styles.boldletter}>Actualizada: </h2>
-          <ListItemText primary={formatDate(details.updateDate)} />
-        </ListItem> */}
+        <div
+          style={{
+            width: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "10px"
+          }}>
+          <img className={styles.image} src={ImageNotFound} alt='imagen' />
+        </div>
+        <div style={{ textAlign: "left", paddingLeft: 10 }}>
+          <h2>{details.descriptionPost}.</h2>
+        </div>
+        <div style={{ textAlign: "left", paddingLeft: 10 }}>
+          <h2>$ {details.price}</h2>
+        </div>
+        <div style={{display:'flex',justifyContent:'center', marginTop:3,marginBottom:3}}>
+          <Button href='https://api.whatsapp.com/send?phone=50762250666' variant="contained" color="primary" onClick={() => {
+            // window.history.push('https://api.whatsapp.com/send?phone=50762250666')
+            // {`https://api.whatsapp.com/send?phone=${details.phoneNumber}`}
+            console.log('Que noris');
+          }}>Contactar</Button>
+        </div>
+        <Separator />
+        <div style={{ padding: 20 }}>
+          <h2 style={{ textAlign: "center" }}>Detalles del listado.</h2>
+          <div className={styles.row}>
+            <h2 className={styles.boldletter}>Categoria: </h2>
+            <h2>{details.category}</h2>
+          </div>
+          <div className={styles.row}>
+            <h2 className={styles.boldletter}>Direccion: </h2>
+            <h2>{details.address}</h2>
+          </div>
+          {/* <div className={styles.row}>
+            <h2 className={styles.boldletter}>Telefono: </h2>
+            <h2>{details.phoneNumber}</h2>
+          </div> */}
 
-        <div className={styles.row}>
-          <h2 className={styles.boldletter}>Categoria: </h2>
-          <h2>{details.category}</h2>
-        </div>
-        <div className={styles.row}>
-          <h2 className={styles.boldletter}>Direccion: </h2>
-          <h2>{details.address}</h2>
-        </div>
-        <div className={styles.row}>
-          <h2 className={styles.boldletter}>Telefono: </h2>
-          <h2>{details.phoneNumber}</h2>
-        </div>
-        <div className={styles.row}>
-          <h2 className={styles.boldletter}>Precio: </h2>
-          <h2>{details.price}</h2>
-        </div>
-        <div style={{paddingBottom:10}} className={styles.row}>
-          <h2 className={styles.boldletter}>Descripcion: </h2>
-          <h2 style={{ top: 15, position: "relative" }}>
-            {details.descriptionPost}
-          </h2>
-        </div>
-        <div className={styles.row}>
-          <h2 className={styles.boldletter}>Creada: </h2>
-          <h2>{formatDate(details.createDate)}</h2>
-        </div>
-        <div className={styles.row}>
-          <h2 className={styles.boldletter}>Actualizada: </h2>
-          <h2>{formatDate(details.updateDate)}</h2>
+          <div className={styles.row}>
+            <h2 className={styles.boldletter}>Creada: </h2>
+            <h2>{formatDate(details.createDate)}</h2>
+          </div>
+          <div className={styles.row}>
+            <h2 className={styles.boldletter}>Actualizada: </h2>
+            <h2>{formatDate(details.updateDate)}</h2>
+          </div>
         </div>
       </div>
     </div>
@@ -82,18 +77,19 @@ export default function Listing(props) {
 const useStyles = makeStyles((theme) => ({
   title: {
     textAlign: "center",
-    marginTop: "50px"
+    marginTop: "15px"
   },
   body: {
-    height: "85vh",
-    width: "100%",
-    padding: 20
+    height: "100%",
+    width: "100%"
+    // padding: 20
   },
   row: {
     display: "flex",
     flexDirection: "row",
     // justifyContent: "space-between",
     alignItems: "center",
+    alignContent: "center",
     marginTop: "20px",
     marginBottom: "20px"
   },
@@ -102,5 +98,11 @@ const useStyles = makeStyles((theme) => ({
     fontSize: "20px",
     color: "black",
     marginRight: "10px"
+  },
+  image: {
+    width: "100%",
+    height: "40vh",
+    marginBottom: "20px",
+    objectFit: "cover"
   }
 }));
---
+++ src/components/common/separator.component.js
@@ -0,0 +1,22 @@
+/** @format */
+
+import React from "react";
+
+const Separator = () => {
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div
+        style={{
+          border: 1,
+          width: "70vw",
+          height: 1,
+          backgroundColor: "black",
+          marginTop: 10,
+          marginBottom: 10
+        }}
+      />
+    </div>
+  );
+};
+
+export default Separator;
